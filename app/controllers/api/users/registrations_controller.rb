051ee60d39bdbc1c42d6fb8c54968d48be4a34e6
Saravanan <sivasaravanan9578@gmail.com>
2025-05-31 16:29:07 +0530

    Add GitHub Actions deploy.yml for backend CI/CD

--- app/controllers/api/users/registrations_controller.rb
+++ app/controllers/api/users/registrations_controller.rb
@@ -1,21 +1,10 @@
 class Api::Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
-  # Strong parameters - must permit user params explicitly
-  private
-
-  def sign_up_params
-    
-    user_data = params[:user] || params.dig(:registration, :user)
-    user_data ||= {}
-
-    ActionController::Parameters.new(user_data).permit(:email, :password, :password_confirmation)
-  end
-
-  public
+  before_action :configure_sign_up_params, only: [:create]
 
   def create
-    Rails.logger.info "Params received: #{params.inspect}"  # Add this line
+    Rails.logger.info "Params received: #{params.inspect}"
 
     build_resource(sign_up_params)
 
@@ -32,4 +21,16 @@ class Api::Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
+  end
+
+  def sign_up_params
+    user_data = params[:user] || params.dig(:registration, :user)
+    user_data ||= {}
+
+    ActionController::Parameters.new(user_data).permit(:email, :password, :password_confirmation)
+  end
 end
