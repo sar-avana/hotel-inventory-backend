67c9f143b1af8b2674098aef02f465f179f1a59d
Saravanan <sivasaravanan9578@gmail.com>
2025-05-31 12:31:22 +0530

    Add GitHub Actions deploy.yml for backend CI/CD

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users,
-  defaults: { format: :json },
-  path: '',
-  path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
-  },
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
-  get '/products', to: 'products#index'
+  namespace :api, defaults: { format: :json } do
+    devise_for :users,
+      path: '',
+      path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'signup'
+      },
+      controllers: {
+        sessions: 'api/users/sessions',
+        registrations: 'api/users/registrations'
+      }
 
+    get '/products', to: 'products#index'
+  end
 end
+
